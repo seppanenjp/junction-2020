f6e8727eab440f40c3f8a717b468eec884676b97
Ville <ville1.nevasuo@gmail.com>
2020-11-07 20:34:06 +0200

    Add foodtype table & create more db data

--- backend/src/fixtures/generate.ts
+++ backend/src/fixtures/generate.ts
@@ -1,6 +1,13 @@
 import { Restaurant } from '../entities/restaurant';
-
+import { FoodType } from '../entities/foodType';
 import { v4 as uuid } from 'uuid';
+
+export const generateFoodType = (
+  id,
+  name
+): FoodType => {
+  return {id, name};
+}
 
 export const generateRestaurant = (
   name,
