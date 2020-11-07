596a377cde0b448ca5aa5dabc48563e96db06b35
Santeri Suitiala <santeri.suitiala@voyantic.com>
2020-11-07 23:07:00 +0200

    add /foodtypes

---
+++ backend/src/controllers/foodtypes.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from "typeorm";
+import { FoodTypeRepository } from "../repositories/foodType";
+
+export const foodTypeController = require('express').Router();
+
+foodTypeController.get('/', async (request: Request, response: Response) => {
+    const foodTypeRepository: FoodTypeRepository = getCustomRepository(
+        FoodTypeRepository
+    );
+    foodTypeRepository
+        .find()
+        .then((foodTypes) => {
+            response.send(foodTypes)
+        });
+});
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router, Request, Response } from 'express';
 import { lunchController } from '../controllers/lunch';
 import { choiceController } from '../controllers/choice';
 import { createPostGresData } from '../fixtures/data';
+import { foodTypeController } from '../controllers/foodtypes';
 
 export const routes = Router();
 
 routes.use('/lunch', lunchController);
 routes.use('/choice', choiceController);
+routes.use('/foodtypes', foodTypeController);
 
 routes.use('/fixtures', (request: Request, response: Response) => {
   createPostGresData();
@@ -15,9 +17,4 @@ routes.use('/fixtures', (request: Request, response: Response) => {
 
 routes.use('/', (request: Request, response: Response) => {
   response.send({ info: 'Luncher backend' });
-});
-
-routes.use('/fixtures', (request: Request, response: Response) => {
-  createPostGresData();
-  response.send({ info: 'Fixtures created' });
 });
