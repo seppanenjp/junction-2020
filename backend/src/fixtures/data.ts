ef609c54782f01c13de33bd68054b2367a4f1f69
Jukka-Pekka <jukka-pekka.seppanen@vincit.fi>
2020-11-07 22:59:52 +0200

    PG data fixtures

--- backend/src/fixtures/data.ts
+++ backend/src/fixtures/data.ts
@@ -3,22 +3,53 @@ import { FoodTypeRepository } from '../repositories/foodType';
 import { RestaurantRepository } from '../repositories/restaurant';
 import { generateRestaurant, generateFoodType } from './generate';
 
-export const createPostGresData = () => {
+export const createPostgresData = async () => {
   const restaurantRepository: RestaurantRepository = getCustomRepository(
     RestaurantRepository
   );
-  restaurantRepository.save([
+  await restaurantRepository.save([
     generateRestaurant('Barbarossa', 1, 60.168265, 24.930987, 'Pizzaa'),
-    generateRestaurant('Singapore Hot Wok', 2, 60.169161, 24.933669, 'Numero 1, Numero 2, Numero 3'),
-    generateRestaurant('McDonalds', 1, 60.169005, 24.929982, 'Big Mac & Cheesburger'),
-    generateRestaurant('Noodle Master', 2, 60.171396, 24.926701, 'Dan Dan noodles'),
-    generateRestaurant('Classic Pizza Stockmann', 2, 60.168415, 24.942684, 'Best Pizza in town'),
-    generateRestaurant('Ravintola Paulette', 2, 60.158708, 24.945918, 'French cuisine')]);
+    generateRestaurant(
+      'Singapore Hot Wok',
+      [1, 2],
+      60.169161,
+      24.933669,
+      'Numero 1, Numero 2, Numero 3'
+    ),
+    generateRestaurant(
+      'McDonalds',
+      [1],
+      60.169005,
+      24.929982,
+      'Big Mac & Cheesburger'
+    ),
+    generateRestaurant(
+      'Noodle Master',
+      [2],
+      60.171396,
+      24.926701,
+      'Dan Dan noodles'
+    ),
+    generateRestaurant(
+      'Classic Pizza Stockmann',
+      [2, 3],
+      60.168415,
+      24.942684,
+      'Best Pizza in town'
+    ),
+    generateRestaurant(
+      'Ravintola Paulette',
+      [2, 4],
+      60.158708,
+      24.945918,
+      'French cuisine'
+    )
+  ]);
 
   const foodTypeRepository: FoodTypeRepository = getCustomRepository(
     FoodTypeRepository
   );
-  foodTypeRepository.save([
+  await foodTypeRepository.save([
     generateFoodType(0, 'Pizza'),
     generateFoodType(1, 'Sushi'),
     generateFoodType(2, 'Thai'),
@@ -39,5 +70,6 @@ export const createPostGresData = () => {
     generateFoodType(17, 'American'),
     generateFoodType(18, 'Ramen'),
     generateFoodType(19, 'Falafel'),
-    generateFoodType(20, 'Eco')]);
+    generateFoodType(20, 'Eco')
+  ]);
 };
