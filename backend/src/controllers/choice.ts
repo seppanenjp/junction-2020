ed694e376e19caee4ce0b6e721e1049dfda4ea77
Jukka-Pekka <jukka-pekka.seppanen@vincit.fi>
2020-11-07 19:25:53 +0200

    Split body

--- backend/src/controllers/choice.ts
+++ backend/src/controllers/choice.ts
@@ -4,6 +4,11 @@ export const choiceController = require('express').Router();
 
 choiceController.post('/', async (request: Request, response: Response) => {
   const choice = request.body;
+
+  const foodType: number[] = choice.foodType;
+  const result: number = choice.result;
+  const participantId: string = choice.participantId;
+
   // TODO: save to participant here
 
   response.send({ choices: [4, 2] });
