1d74ddabfef7a346d0f49321536b31799ecd761c
Jukka-Pekka Seppänen <46344971+seppanenjp@users.noreply.github.com>
2020-11-07 11:10:38 +0200

    Typeorm init (#4)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,43 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as prerender from "prerender-node";
+import * as compression from "compression";
+import "reflect-metadata";
+import "colors";
+import { createConnection } from "typeorm";
+import { config } from "dotenv";
+
 const app = express();
-const port = 8080; // default port to listen
+config();
+const port = process.env.PORT || 8080;
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.use(compression());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(prerender);
 
-// start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+export const connection = createConnection({
+  type: "postgres",
+  username: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: Number(process.env.DB_PORT),
+  entities: [],
+  migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
+  cli: {
+    migrationsDir: "src/migrations",
+  },
+  migrationsTableName: "Migrations",
+  synchronize: false,
+  migrationsRun: true,
+  logging: false,
+}).then(() => {
+  app.get("/", (req, res) => {
+    res.send("Hello world!");
+  });
+
+  // start the Express server
+  app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`.green);
+  });
+});
