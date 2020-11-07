0bb220fbb56aa4d8db194cefe44999b0d144531a
Jukka-Pekka <jukka-pekka.seppanen@vincit.fi>
2020-11-07 11:12:39 +0200

    prettierrc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,39 +1,39 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import * as prerender from "prerender-node";
-import * as compression from "compression";
-import "reflect-metadata";
-import "colors";
-import { createConnection } from "typeorm";
-import { config } from "dotenv";
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as prerender from 'prerender-node';
+import * as compression from 'compression';
+import 'reflect-metadata';
+import 'colors';
+import { createConnection } from 'typeorm';
+import { config } from 'dotenv';
 
 const app = express();
 config();
 const port = process.env.PORT || 8080;
 
 app.use(compression());
-app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(prerender);
 
 export const connection = createConnection({
-  type: "postgres",
+  type: 'postgres',
   username: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: Number(process.env.DB_PORT),
   entities: [],
-  migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   cli: {
-    migrationsDir: "src/migrations",
+    migrationsDir: 'src/migrations'
   },
-  migrationsTableName: "Migrations",
+  migrationsTableName: 'Migrations',
   synchronize: false,
   migrationsRun: true,
-  logging: false,
+  logging: false
 }).then(() => {
-  app.get("/", (req, res) => {
-    res.send("Hello world!");
+  app.get('/', (req, res) => {
+    res.send('Hello world!');
   });
 
   // start the Express server
